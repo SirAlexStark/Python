adcb45195bb1dfe9cd728c815e0bc66a80a931b7
SirAlexStark <53335934+SirAlexStark@users.noreply.github.com>
2020-11-19 00:00:22 -0300

    Add files via upload

---
+++ guessing_game.py
@@ -0,0 +1,79 @@
+import random
+def validacao ():
+
+   print('************************************************************************************************')
+   print("Olá seja bem vindo ao jogo da adivinhação, minha tarefa é acertar o número que você escolher")
+   print("***********************************************************************************************")
+
+   numero_secreto = int(input("Qual é o número secreto ? "))
+   chute = random.randrange(10, 20)
+   maior = chute > numero_secreto
+   menor = chute < numero_secreto
+
+
+   total_tentativas = 1
+
+
+   while (total_tentativas <= 5):
+
+     chute = random.randrange(10, 20)
+     valida = input("O número secreto é o {}, acertei ?".format(chute)).lower()
+     acertei = numero_secreto == chute
+     print(total_tentativas)
+
+     if (acertei and valida == "sim"):
+          print("Acertei, fim de jogo")
+          break
+
+     elif maior:
+          print("Poxa meu chute foi acima do Numero secreto")
+          total_tentativas = total_tentativas + 1
+
+     elif menor:
+          print("Poxa meu chute foi abaixo do numero secreto")
+          total_tentativas = total_tentativas + 1
+
+     if(total_tentativas == 5):
+         print("Desisto, perdi o jogo")
+
+
+       
+
+if(__name__ == '__main__'):
+    validacao()
+
+
+
+
+
+ 
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
